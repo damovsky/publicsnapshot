b249ee4297fec1923dcace754e1d7156aba0d3cc
Martin Damovsky <martin.damovsky@ataccama.com>
2024-09-06 22:41:17 +0200

    Initial commit

---
+++ test/public_snapshots.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PublicSnapshots from '../lib/public_snapshots-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/public_snapshots-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PublicSnapshots.PublicSnapshotsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
